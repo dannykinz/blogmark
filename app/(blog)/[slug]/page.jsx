dcccf80b5d8161c3a246ee062fa574877fb7567b
Danny Kinz <talktodknow@gmail.com>
2025-11-18 13:03:23 +0100

    Dynamic routing page single blog page fixed.

--- app/(blog)/[slug]/page.jsx
+++ app/(blog)/[slug]/page.jsx
@@ -1,10 +1,30 @@
-import Heading from '@/app/components/Heading';
+import { notFound } from 'next/navigation';
+import { getBlogPosts, getPostSlug } from '@/app/lib/posts';
 
-const SinglePost = ({ slug }) => {
+export const generateStaticParams = () => {
+    const posts = getBlogPosts();
+
+    return posts.map(post => ({
+        slug: post.slug,
+    }));
+};
+
+const SinglePost = async ({ params }) => {
+    const { slug } = await params;
+
+    const post = getPostSlug(slug);
+
+    if (!post) {
+        notFound();
+    }
+
     return (
-        <article>
-            <Heading text={slug} />
-        </article>
+        <main className="mt-[80px] space-y-6">
+            <h1 className="text-4xl text-center text-blue-400 font-extrabold md:text-8xl">
+                {post.title}
+            </h1>
+            <p className="text-lg leading-relaxed text-zinc-200 text-center">{post.body}</p>
+        </main>
     );
 };
 
